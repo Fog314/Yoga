08875751536b8c320be1074c72c51c94a16311ed
Fog314 <navimed7@gmail.com>
2020-02-03 16:45:41 +0300

    Commit 03.02.2020

--- webpack.config.js
+++ webpack.config.js
@@ -26,11 +26,11 @@ module.exports = {
             presets: [
               ["@babel/env", {
                 targets: {
-                  edge: "17",
-                  firefox: "60",
-                  chrome: "67",
-                  safari: "11.1",
-                  ie: "11"
+                  edge: "15",
+                  firefox: "55",
+                  chrome: "60",
+                  safari: "10",
+                  ie: "10"
                 },
                 useBuiltIns: "usage"
               }]
